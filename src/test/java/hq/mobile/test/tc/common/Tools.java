d38b90b7e0ba09e932b7f418ab02a98c3aa47cb1
hq11258 <hq11258@ly.com>
2015-05-13 14:54:04 +0800

    common：修改截图方法，直接输出到根目录

--- src/test/java/hq/mobile/test/tc/common/Tools.java
+++ src/test/java/hq/mobile/test/tc/common/Tools.java
@@ -33,7 +33,7 @@ public class Tools {
             SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
             String timeStr = df.format(new Date());
             File screenShotFile = d.getScreenshotAs(OutputType.FILE);
-            String fileFullPath = String.format("%s//screenshot//%s_%s.%s", filePath, fileName, timeStr, extName);
+            String fileFullPath = String.format("%s//%s_%s.%s", filePath, fileName, timeStr, extName);
             FileUtils.copyFile(screenShotFile, new File(fileFullPath));
             log(String.format("截图文件名：%s_%s.%s", fileName, timeStr, extName));
         } catch (Exception e) {
